0ed7781ce94d3e05dcb0b4195174df01116565b5
Mihai Bîrsan <alexandru.m.birsan@gmail.com>
2023-06-12 13:45:11 +0300

    WIP: Index

--- src/ExpertPotatoService.ts
+++ src/ExpertPotatoService.ts
@@ -1,7 +1,12 @@
 import { App, Plugin, TAbstractFile } from "obsidian";
 
+type IndexData = {
+    [key: string]: {},
+}
+
 export class ExpertPotatoService {
-    private indexedFiles = new Set<string>();
+    private indexedFiles: IndexData = {};
+    private indexPath = `${this.app.vault.configDir}/plugins/${this.plugin.manifest.id}/index.json`;
 
     constructor(
         private app: App,
@@ -9,6 +14,8 @@ export class ExpertPotatoService {
     ) {}
 
     async onload() {
+        await this.loadIndex();
+
         // NOTE: When the app is starting or is being reloaded,
         // a "create" event is fired for every file in the vault
         this.plugin.registerEvent(this.app.vault.on("create", this.createFile));
@@ -16,46 +23,73 @@ export class ExpertPotatoService {
         this.plugin.registerEvent(this.app.vault.on("rename", this.renameFile));
         this.plugin.registerEvent(this.app.vault.on("delete", this.deleteFile));
         // NOTE: The "closed" event doesn't seem to be useful here
-
-        // TODO: This information will need to be persisted in the plugin setings directory
-        const indexPath = `${this.app.vault.configDir}/${this.plugin.manifest.id}/index.json`;
-        const indexArray: string[] = JSON.parse(await this.app.vault.adapter.read(indexPath))
-        this.indexedFiles.clear();
-        indexArray.forEach(path => this.indexedFiles.add(path));
-        // NOTE: Can we get a list of all indexed files from the service? (In case this one forgets)
-        // Would it be a NOOP to upload a file that's already be indexed?
     }
 
-    onunload() {
-        // TODO: persist index
-        this.indexedFiles.clear();
+    async onunload() {
+        this.saveIndex();
     }
 
     createFile = (file: TAbstractFile) => {
-        this.indexedFiles.add(file.path);
+        this.indexedFiles[file.path] = {};
+        this.saveIndexDelayed();
         console.log("createFile", `${file.path}`);
-        console.log(JSON.stringify(Array.from(this.indexedFiles), null, 2));
         // NOTE: Create and Modify are similar, since a file may be copied from a different souce
     }
 
     modifyFile = (file: TAbstractFile) => {
-        this.indexedFiles.add(file.path);
+        this.indexedFiles[file.path] = {};
+        this.saveIndexDelayed();
         console.log("modifyFile", `${file.path}`);
         // NOTE: Defer event handling to the service
     }
 
     renameFile = (file: TAbstractFile, oldPath: string) => {
-        this.indexedFiles.delete(oldPath);
-        this.indexedFiles.add(file.path);
-        // ERROR: there's no information about the previous name :/
+        this.indexedFiles[file.path] = this.indexedFiles[oldPath];
+        delete this.indexedFiles[oldPath];
+        this.saveIndexDelayed();
         console.log("renameFile", `${oldPath} -> ${file.path}`);
         // NOTE: This would ideally only update the metadata of the file;
         // otherwise, it's a delete and create
     }
 
     deleteFile = (file: TAbstractFile) => {
-        this.indexedFiles.delete(file.path);
+        delete this.indexedFiles[file.path];
+        this.saveIndexDelayed();
         console.log("deleteFile", `${file.path}`);
+    }
+
+    async loadIndex() {
+        if (await this.app.vault.adapter.exists(this.indexPath, true)) {
+            this.indexedFiles = JSON.parse(await this.app.vault.adapter.read(this.indexPath));
+            console.log('Loaded index', this.indexedFiles);
+        } else {
+            console.log('No index found');
+        }
+    }
+
+    private saveIndexDelayedTimeout: ReturnType<typeof setTimeout> | null = null;
+    async saveIndexDelayed() {
+        if (this.saveIndexDelayedTimeout) {
+            clearTimeout(this.saveIndexDelayedTimeout);
+            this.saveIndexDelayedTimeout = null;
+        }
+        return new Promise<void>((resolve, reject) => {
+            this.saveIndexDelayedTimeout = setTimeout(async () => {
+                this.saveIndexDelayedTimeout = null;
+                await this.app.vault.adapter.write(this.indexPath, JSON.stringify(this.indexedFiles));
+                console.log('Saved index (delayed)', this.indexedFiles);
+                resolve();
+            }, 2000);
+        });
+    }
+
+    async saveIndex() {
+        if (this.saveIndexDelayedTimeout) {
+            clearTimeout(this.saveIndexDelayedTimeout);
+            this.saveIndexDelayedTimeout = null;
+        }
+        await this.app.vault.adapter.write(this.indexPath, JSON.stringify(this.indexedFiles));
+        console.log('Saved index', this.indexedFiles);
     }
 
     // async search(query) {
